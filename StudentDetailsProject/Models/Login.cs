48f6e063e091e5b06f5c20b0af7dbe623f20f7e8
saleema np <saleemaperidot@gmail.com>
2022-10-16 19:33:04 +0530

    Login and registration compleleted filter by date to be done

--- StudentDetailsProject/Models/Login.cs
+++ StudentDetailsProject/Models/Login.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
+using System.Xml.Linq;
 
 namespace StudentDetailsProject.Models
 {
     public class Login
     {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string usrname { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [MembershipPassword(
+    MinRequiredNonAlphanumericCharacters = 1,
+    MinNonAlphanumericCharactersError = "Your password needs to contain at least one symbol (!, @, #, etc).",
+    ErrorMessage = "Your password must be 6 characters long and contain at least one symbol (!, @, #, etc)."
+)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+
         public string password { get; set; }
     }
 }
--- StudentDetailsProject/Models/studentModel.cs
+++ StudentDetailsProject/Models/studentModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 
 namespace StudentDetailsProject.Models
 {
@@ -14,9 +15,20 @@ namespace StudentDetailsProject.Models
         public string LastName { get; set; }
         [Required]
         public string FirstName { get; set; }
-        [Required]
+       
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string StudentUserName { get; set; }
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [MembershipPassword(
+    MinRequiredNonAlphanumericCharacters = 1,
+    MinNonAlphanumericCharactersError = "Your password needs to contain at least one symbol (!, @, #, etc).",
+    ErrorMessage = "Your password must be 6 characters long and contain at least one symbol (!, @, #, etc)."
+)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
         public string StudentPassword { get; set; }
         [Required]
         public string FathersName { get; set; }
@@ -24,11 +36,14 @@ namespace StudentDetailsProject.Models
         public string StudentAddress { get; set; }
         [Required]
         public string City { get; set; }
-        [Required]
+        [Required(ErrorMessage = "You must provide a phone number")]
+        [Display(Name = "Home Phone")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
         public string ContactNumber { get; set; }
-        [Required]
+        [Required(ErrorMessage = "date format 1111-11-11")]
         public DateTime DateOfbirth { get; set; }
-        [Required]
+        [Required(ErrorMessage = "date format 1111-11-11")]
         public DateTime DateOfJoining { get; set; }
 
 
